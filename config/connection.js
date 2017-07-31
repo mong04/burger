6b7eed02dd6c0f65f813d06649ae4cb83f91f688
Andrew Sanchez <bballbizniz@aol.com>
2017-07-31 13:12:28 -0700

    removed redundant code

--- config/connection.js
+++ config/connection.js
@@ -11,25 +11,6 @@ if (process.env.JAWSDB_URL) {
     });
 };
 
-// var connection = mysql.createConnection({
-//     port: 3306,
-//     host: "localhost",
-//     user: "root",
-//     password: "Fightingpadre!$",
-//     database: "burgers_db"
-// });
-
-// if (process.env.JAWSDB_URL) {
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//     connection = mysql.createConnection({
-//         host: 'localhost',
-//         user: 'root',
-//         password: 'Fightingpadre!$',
-//         database: 'burgers_db'
-//     })
-// }
-
 connection.connect(function(err) {
     if (err) {
         console.error("error connecting: " + err.stack);
